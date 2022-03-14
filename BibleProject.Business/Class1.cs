0f535c4173f93c18fc873450bf6a34e56e72ffc0
Chase Hard <dchasehardin@gmail.com>
2022-03-14 18:49:19 -0500

    initial commit

---
+++ BibleProject.Business/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace BibleProject.Business
+{
+    public class Class1
+    {
+    }
+}
---
+++ BibleProject.Repository/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace BibleProject.Repository
+{
+    public class Class1
+    {
+    }
+}
